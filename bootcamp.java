8e47c6995d938c928b57784f8b470e0717b50bef
Irã Guimarães <iranmcg016@gmail.com>
2026-05-23 20:18:22 -0300

    Remoção de comentários, de variável solta e de print dessa variável.

--- bootcamp.java
+++ bootcamp.java
@@ -3,20 +3,17 @@ import java.lang.*;
 public class bootcamp{
     public static void main(String[] args){
 
-//        System.out.println("=== ATENDIMENTO DA CAFETERIA ===\n");
-//        System.out.println
-//                ("Segue abaixo nosso cardápio:" +
-//                        "1 - CAFÉ PRETO (R$ 3,00)" +
-//                        "2 - CAFÉ COM LEITE (R$ 4,00)" +
-//                        "3 - CAPUCCINO (R$ 7,00)" +
-//                        "4 - PÃO COM MANTEIGA (R$ 2,50)" +
-//                        "5 - MISTO QUENTE (R$ 5,00)");
-//
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("Escolha um número correspondente a um dos itens do cardápio: ");
-//        String produto = input.nextLine();
+        System.out.println("=== ATENDIMENTO DA CAFETERIA ===\n");
+        System.out.println
+                ("Segue abaixo nosso cardápio:" +
+                        "1 - CAFÉ PRETO (R$ 3,00)" +
+                        "2 - CAFÉ COM LEITE (R$ 4,00)" +
+                        "3 - CAPUCCINO (R$ 7,00)" +
+                        "4 - PÃO COM MANTEIGA (R$ 2,50)" +
+                        "5 - MISTO QUENTE (R$ 5,00)");
 
-        String nome = "Irã";
-            System.out.print("Meu nome é %s".formatted(nome));
+        Scanner input = new Scanner(System.in);
+        System.out.print("Escolha um número correspondente a um dos itens do cardápio: ");
+        String produto = input.nextLine();
     }
 }
