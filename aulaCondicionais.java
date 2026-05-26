a2b3a47bf4c83a02dfffd88664a75c2bb07ff8a6
Irã Guimarães <iranmcg016@gmail.com>
2026-05-26 00:56:06 -0300

    Cria condicional do plano do filme

--- aulaCondicionais.java
+++ aulaCondicionais.java
@@ -5,10 +5,19 @@ public class aulaCondicionais {
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
 
+        //Condicional do ano de lançamento
         if (anoDeLancamento > 2022){
             System.out.println("Lançamento que os clientes estão curtindo");
         } else {
             System.out.println("Filme retrô que vale a pena assistir");
+        }
+
+        //Condicional do plano do filme
+        String tipoPlano = "plus";
+        if (incluidoNoPlano == true || tipoPlano.equals("plus")) {
+            System.out.println("Filme liberado");
+        } else  {
+            System.out.println("Deve pagar a locação");
         }
     }
 }
