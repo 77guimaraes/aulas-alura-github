c7382cb3d58075d1a2fad23cacbdec448f2e9e91
Irã Guimarães <iranmcg016@gmail.com>
2026-05-27 00:22:08 -0300

    Cria arquivo de loop com while

---
+++ aulaLoop2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class aulaLoop2 {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner (System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+        int totalDeNotas = 0;
+
+        //enquanto i
+        while (nota != -1) {
+            System.out.println("Diga sua avaliação sobre o filme ou -1 para encerrar: ");
+            nota = leitura.nextDouble();
+
+            if (nota != -1) {
+                mediaAvaliacao = mediaAvaliacao + nota;
+                totalDeNotas = totalDeNotas + 1;
+            }
+
+        }
+
+        System.out.println("Media de avaliações: " + mediaAvaliacao / totalDeNotas);
+
+
+
+    }
+}
