f9d1dcb1acb50e805d12234b9fca5532385c1c72
Irã Guimarães <iranmcg016@gmail.com>
2026-05-27 00:12:05 -0300

    Cria loop de avaliação de filme

--- aulaLoop.java
+++ aulaLoop.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 
 public class aulaLoop {
     public static void main(String[] args) {
-        Scanner leitura = new Scanner (System.in);
 
-        for (int i = 0; i < ; i++) {
-            
+        /* O programa abaixo recebe até 03 avaliações de notas do filme
+        e depois calcula a media de avaliação do filme avaliado
+        */
+
+        Scanner leitura = new Scanner (System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        //enquanto i for igual a 0 e i menor que X adicione mais um
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Diga sua avaliação sobre o filme: ");
+            nota = leitura.nextDouble();
+            mediaAvaliacao += nota; //recebe o que ja tinha + a nota
         }
-        
-        
-        
-        //Pede avaliação do filme
-        System.out.println("Diga sua avaliação sobre o filme: ");
-        double avaliacao = leitura.nextDouble();
+
+        System.out.println("Media de avaliações" + mediaAvaliacao/3);
+
+
 
     }
 }
