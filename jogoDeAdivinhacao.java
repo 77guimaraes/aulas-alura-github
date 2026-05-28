7bf8304c358ede7246a348b903a2348898a30bda
Irã Guimarães <iranmcg016@gmail.com>
2026-05-27 21:20:09 -0300

    Cria arquivo com exercicio de loop e leitura de dados

---
+++ jogoDeAdivinhacao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class jogoDeAdivinhacao {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+    }
+}
