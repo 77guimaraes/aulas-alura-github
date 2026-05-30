4761066413df5848472c52456827415f785f0097
Irã Guimarães <iranmcg016@gmail.com>
2026-05-30 16:14:32 -0300

    Cria arquivo desafioBancario.java com dados do cliente

---
+++ desafioBancario.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class desafioBancario {
+    public static void main(String[] args) {
+
+        //Inicializa dados do cliente
+        String nome = "Bruce";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+
+
+    }
+}
