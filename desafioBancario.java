c4ca7246bc0799adbe4d6f3879dba9a68b938ce0
Irã Guimarães <iranmcg016@gmail.com>
2026-05-30 16:44:50 -0300

    Mostra/printa informações do usuario

--- desafioBancario.java
+++ desafioBancario.java
@@ -6,6 +6,15 @@ public class desafioBancario {
         String nome = "Bruce";
         String tipoConta = "Corrente";
         double saldo = 1599.99;
+        int opcao = 0;
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("*********************");
+        System.out.println("Nome do cliente: " + nome);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("*********************");
 
         String menu = """
                 \nDigite o numero da opção que deseja:
@@ -14,9 +23,7 @@ public class desafioBancario {
                 2 - Transferir Valor;
                 3 - Receber Valor;
                 4 - Sair.""";
-
-        int opcao = 0;
-        Scanner leitura = new Scanner(System.in);
+        System.out.println(menu);
 
         while (opcao != 4){
 
